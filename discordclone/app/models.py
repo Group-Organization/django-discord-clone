47503c8d91b3e4702e4fdd88c57052a7f8f5de55
someguyidk <7carloselias@gmail.com>
2022-08-22 13:13:42 -0500

    updated ServerMessage model to include user field

--- discordclone/app/models.py
+++ discordclone/app/models.py
@@ -1,26 +1,33 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 import uuid
+from django.db import models
+from discordclone.userhandler.models import User
 
 # Create your models here.
+
+
 class Server(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=256, blank=True)
     owner = models.ForeignKey('userhandler.User', on_delete=models.CASCADE)
-    participants = models.ManyToManyField('userhandler.User', related_name='participants')
+    participants = models.ManyToManyField(
+        'userhandler.User', related_name='participants')
     roles = models.ManyToManyField('Role', related_name='roles', blank=True)
-    voice_channels = models.ManyToManyField('VoiceChannel', related_name='voice_channels', blank=True)
-    text_channels = models.ManyToManyField('TextChannel', related_name='text_channels', blank=True)
-    messages = models.ManyToManyField('Message', related_name='messages', blank=True)
+    voice_channels = models.ManyToManyField(
+        'VoiceChannel', related_name='voice_channels', blank=True)
+    text_channels = models.ManyToManyField(
+        'TextChannel', related_name='text_channels', blank=True)
+    messages = models.ManyToManyField(
+        'Message', related_name='messages', blank=True)
     logo = models.ImageField(blank=True)
     created = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.name
 
+
 class Role(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30)
     server = models.ForeignKey('Server', on_delete=models.CharField)
     color = models.CharField(max_length=10)
@@ -28,9 +35,11 @@ class Role(models.Model):
     def __str__(self):
         return self.name
 
+
 class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey('userhandler.User', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(
+        'userhandler.User', on_delete=models.SET_NULL, null=True)
     message = models.TextField(max_length=500)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
@@ -38,20 +47,24 @@ class Message(models.Model):
     def __str__(self):
         return self.message
 
-class ServerMessage(Message):
-    channel = models.ForeignKey('TextChannel', on_delete=models.CASCADE)
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
-
 
 class TextChannel(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, unique=True)
     name = models.CharField(max_length=64, null=True)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, null=True)
-    participants = models.ManyToManyField('userhandler.User', related_name='ChannelParticipants', blank=True)
+    participants = models.ManyToManyField(
+        'userhandler.User', related_name='ChannelParticipants', blank=True)
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
+
 class VoiceChannel(TextChannel):
     pass
+
+
+class ServerMessage(Message):
+    channel = models.ForeignKey(TextChannel, on_delete=models.CASCADE)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET('User Deleted'))
