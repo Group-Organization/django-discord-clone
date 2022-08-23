08e34877102ea9048093ef1d08c2d35669b6befc
Snowine <gamerkidben89@gmail.com>
2022-08-23 15:01:07 +0300

    Added pending to models, edited friends view, added add friends and remove friends view

--- discordclone/app/urls.py
+++ discordclone/app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('friends/', views.friends, name='friends'),
+    path('friends/add-friend/<str:friendUsername>',
+         views.addFriend, name='addFriend'),
+    path('friends/removeFriend/<str:friendUsername>',
+         views.removeFriend, name='removeFriend')
 ]
--- discordclone/app/views.py
+++ discordclone/app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from userhandler.models import User
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -22,19 +24,9 @@ def server(request):
 @login_required(login_url='login')
 def friends(request):
     user = User.objects.get(email=request.user.email)
-    userFriendList = user.friend.all()
+    friends = user.friendlist.all()
     blocked_users = user.blocked_users.all()
-    pending = []
-    friends = []
-
-    # Checks if the person is blocked, friends or only a one sided friend relationship meaning pending.
-    for people in userFriendList:
-        if people in blocked_users:
-            pass
-        elif not user in people.friend.all():
-            pending.append(people)
-        else:
-            friends.append(people)
+    pending = user.pending_users.all()
 
     context = {
         'friends': friends,
@@ -42,3 +34,38 @@ def friends(request):
         'pending': pending,
     }
     return render(request, 'app/friends.html', context)
+
+
+@login_required(login_url='login')
+def addFriend(request, friendUsername):
+    newFriend = User.objects.filter(username=friendUsername.lower())
+    user = request.user
+    userFriends = user.friendlist.all()
+
+    if not newFriend.exists():
+        messages.error('User does not exist!')
+        return redirect('friends')
+    if newFriend[0] in userFriends:
+        messages.error(request, 'You\'ve already added this user!')
+        return redirect('friends')
+
+    print(newFriend[0])
+    user.friendlist.add(newFriend[0])
+
+    return redirect('friends')
+
+
+def removeFriend(request, friendUsername):
+    friend = User.objects.filter(username=friendUsername.lower())
+    user = request.user
+    userFriends = user.friendlist.all()
+
+    if not friend.exists():
+        messages.error('User does not exist!')
+        return redirect('friends')
+    if not friend[0] in userFriends:
+        messages.error(request, 'You are not friends with this user!')
+        return redirect('friends')
+
+    user.friendlist.remove(friend[0])
+    return redirect('friends')
